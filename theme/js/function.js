3861a16a0726a2a53f3a976d9117c59bb9f018c8
siddharth-techie-vyas <sid.vyas786@gmail.com>
2025-03-28 13:34:53 +0530

    alert hide

--- theme/js/function.js
+++ theme/js/function.js
@@ -214,7 +214,6 @@ function get_mtype(id)
         type:'GET',
         url:base_url+'index.php?action=sales&query=getmtype&mtype='+mtype,
         success:function(result){
-          alert(result);
             $('#material'+id).html(result);
         }
     });
