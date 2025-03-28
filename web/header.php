605640390b2cbb31f884bc98ebebd0eb7d1df2ef
siddharth-techie-vyas <sid.vyas786@gmail.com>
2025-03-28 13:59:07 +0530

    ajax

--- web/header.php
+++ web/header.php
@@ -18,6 +18,7 @@
 	<link rel="stylesheet" href="<?php echo $base_url;?>theme/css/style.css">
 	<link rel="stylesheet" href="<?php echo $base_url;?>theme/css/skin_color.css">
      
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="<?php echo $base_url.'theme/js/jquery-3-7-1.min.js';?>"></script>	  
   </head>
 
