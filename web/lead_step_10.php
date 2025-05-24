2cfbab695ac2fc56d2134dc206c0205b48205429
siddharth-techie-vyas <sid.vyas786@gmail.com>
2025-05-24 15:04:55 +0530

    detailed info

--- web/lead_step_10.php
+++ web/lead_step_10.php
@@ -32,6 +32,59 @@
 
                 $company_profile = $leads->get_company_profile($_GET['id']);?>
 
+                        <h4>Company Research Info</h4>
+                        <table class="table table-bordered">
+                                                <thead>
+                                                <tr>
+													<th>#</th>
+													<th>Title</th>
+													<th>Sub Title</th>
+													<th>Category</th>
+                                                    <th>Sub Category</th>
+												</tr>
+                                                </thead>				
+												<?php
+												$counter=1;
+												$company_details_leads=$leads->get_leads_company_details($_GET['id']);
+                                                if(!$company_details_leads)
+                                                {
+                                                    echo "<tr><td colspan='4'><div class='alert alert-info'>No Data Found</div></td></tr>";
+                                                }
+                                                else{
+												foreach($company_details_leads as $r =>$v)
+												{   
+                                                    if(is_string($value2))
+                                                    {$value2=$company_details_leads[$r]['value2'];}
+                                                    else
+                                                    {$value21=unserialize($company_details_leads[$r]['value2']);
+                                                        foreach($value21 as $o)
+                                                        {
+                                                            $value2.=$o."<br>";
+                                                        }
+                                                    } 
+                                                    
+                                                    
+
+													echo "<tr>";
+														echo "<th>".$counter++."</th>";
+														echo "<td>".$company_details_leads[$r]['details']."</td>";
+														echo "<td>".$company_details_leads[$r]['value1']."</td>";
+														echo "<td>".$value2."</td>";
+                                                        echo "<td>";
+
+                                                        $value3=$company_details_leads[$r]['value3'];
+                                                        if(is_array($value3))
+                                                        {
+                                                            print_r($value3);
+                                                        }
+                                                        echo "</td>";
+													echo "</tr>";
+												}}
+												?>
+											</table>
+
+                                            <hr>
+                                            <h4>Company Profile</h4>
                 <table class="table table-bordered">
                     <tr>
                         <th>Company Name</th>
@@ -133,6 +186,7 @@
                         </td>
                     </tr>
                 </table>
+                
             <?php }?>
         </div>
       </div>
