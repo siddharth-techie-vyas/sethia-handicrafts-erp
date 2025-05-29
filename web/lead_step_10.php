31347a6bd937d95994b205be909bcb6024ad8280
siddharth-techie-vyas <sid.vyas786@gmail.com>
2025-05-29 14:26:01 +0530

    12

--- web/lead_step_10.php
+++ web/lead_step_10.php
@@ -54,7 +54,7 @@
 												foreach($company_details_leads as $r =>$v)
 												{   
                                                     $value12=array();
-                                                    if(unserialize($company_details_leads[$r]['value2']))
+                                                    if(serialize($company_details_leads[$r]['value2']))
                                                     {$value2=$company_details_leads[$r]['value2'];}
                                                     else
                                                     {                                                        
