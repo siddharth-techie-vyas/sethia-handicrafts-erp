081462021a7a0599ab8b383e83100ab983691967
siddharth-techie-vyas <sid.vyas786@gmail.com>
2025-06-07 14:54:38 +0530

    sd

--- web/sales_rfq_2-0.php
+++ web/sales_rfq_2-0.php
@@ -45,7 +45,7 @@
                   
           
           <?php 
-            
+            $sku=$product->getone($items[$row]['pid']);
                 echo $sku[0]['sku'].'<br>';
                 echo '<small class="text-secondary">'.$sku[0]['product_name'].'</small>';
            
