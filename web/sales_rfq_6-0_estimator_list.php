f329734acad7b4d27f03c464bc6cef9cab598142
siddharth-techie-vyas <sid.vyas786@gmail.com>
2025-06-18 17:25:22 +0530

    we

--- web/sales_rfq_6-0_estimator_list.php
+++ web/sales_rfq_6-0_estimator_list.php
@@ -682,14 +682,17 @@ $itemtype=$items[0]['item_type'];
                                                         echo "<td>".$subassembly_details[$skey]['qty']."</td>";
                                                         echo "<td>".$sqft."</td>";
                                                         //-- control sql fr
-                                                        $control_sqft0 = $subassembly_details[$skey]['length'] + $subassembly_details[$skey]['height'];
-                                                        $control_sqft1 = $subassembly_details[$skey]['width']+$subassembly_details[$skey]['height'];
-                                                        $control_sqft2 = $control_sqft0*$control_sqft1*$subassembly_details[$skey]['qty']*2;
-                                                        $control_sqft +=$control_sqft2;
+                                                        $control_sqft0 = ($subassembly_details[$skey]['length']*$subassembly_details[$skey]['width'])/92900;
+                                                        $control_sqft1 = ($subassembly_details[$skey]['length']*$subassembly_details[$skey]['height'])/92900;
+                                                        $control_sqft2 = ($subassembly_details[$skey]['height']*$subassembly_details[$skey]['width'])/92900;
+                                                        $control_sqft3=$control_sqft0+$control_sqft1+$control_sqft2;
+                                                        $control_sqft4 = round($control_sqft3,3)*$subassembly_details[$skey]['qty'];
+                                                        $control_sqft +=$control_sqft4;
+                                                        echo "<td>".$control_sqft4."</td>";
 
                                                     echo "</tr>";
                                                 }   
-                                                echo "<tr><td colspan='6'></td><th>".$total_sqft."</th></tr>";
+                                                echo "<tr><td colspan='6'></td><th>".$total_sqft."</th><th></th></tr>";
                                                 echo "</table>";
                                                 $grand_sqft += $total_sqft;                     
                                             echo "</td>";
